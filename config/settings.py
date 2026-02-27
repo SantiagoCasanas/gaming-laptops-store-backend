0e5000492df0e1453a0559964df6dfb60d89b32e
SantiagoCasanas <santiago01.02.003@gmail.com>
2026-02-27 10:26:54 -0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -20,9 +20,10 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Cargar las variables de entorno desde el archivo .env
-dotenv_path = BASE_DIR / '.env'
-if dotenv_path.exists():
-    load_dotenv(dotenv_path, override=False)
+if os.getenv('RAILWAY_ENVIRONMENT') is None:
+    dotenv_path = BASE_DIR / '.env'
+    if dotenv_path.exists():
+        load_dotenv(dotenv_path, override=False)
 
 
 # Quick-start development settings - unsuitable for production
